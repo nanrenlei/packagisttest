ff2870929084bd708d2820aa585c79007596a79e
v_zhouyongchao <v_zhouyongchao@100tal.com>
2020-10-16 16:10:41 +0800

    添加测试方法

--- src/MyTest.php
+++ src/MyTest.php
@@ -9,7 +9,22 @@ namespace Package;
 
 class MyTest
 {
-    public function test(){
-        var_dump(...[1,2,3]);
+    public function test()
+    {
+        var_dump(...[1, 2, 3]);
+    }
+
+    /**
+     * 这是一个测试
+     * @return array
+     * @author nanrenlei
+     * @date 2020/10/16 16:04
+     */
+    public static function call()
+    {
+        return [
+            'name' => 'Test',
+            'age' => 21
+        ];
     }
 }
