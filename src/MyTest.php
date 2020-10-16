5ff5d2e9cec3581c2010f9878b8708bd165aae09
v_zhouyongchao <v_zhouyongchao@100tal.com>
2020-10-16 15:41:08 +0800

    test

---
+++ src/MyTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author v_zhouyongchao@100tal.com
+ * @date 2020/10/16 15:07
+ */
+
+namespace Package;
+
+
+class MyTest
+{
+    public function test(){
+        var_dump(...[1,2,3]);
+    }
+}
